dd2efd53aa4b9d62a29948a92ea3f6ff81ce2339
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-05-07 01:13:29 -0700

    Added userControllers api routes

--- README.md
+++ README.md
@@ -20,11 +20,14 @@ Required Packages to install
 
 ## [Credits](#table-of-content)
 Without these guidances, my application would not exist.
+18-NOSQL/01-Activities/03-Day/25-Ins-CRUD-Subdoc
+
 * [SchemaTypes](https://mongoosejs.com/docs/schematypes.html)
 * [SchemaTypes: Date](https://mongoosejs.com/docs/schematypes.html#dates)
 * [SchemaTypes: Getters/Setters](https://mongoosejs.com/docs/tutorials/getters-setters.html)
 * [SchemaTypes: Date](https://mongoosejs.com/docs/schematypes.html#dates)
-* [Default](https://mongoosejs.com/docs/defaults.html)
+* [SchemaTypes: Default](https://mongoosejs.com/docs/defaults.html)
+* [Models: Delete many](https://mongoosejs.com/docs/api/model.html#Model.deleteMany())
 * [How to Convert Timestamp to Date Format in JavaScript](https://linuxhint.com/convert-timestamp-to-date-format-javascript/)
 * [Date.prototype.getFullYear()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getFullYear)
 * [Conditional (ternary) operator](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator)
---
+++ controllers/userControllers.js
@@ -0,0 +1,74 @@
+const {User, Thought} = require("../models");
+
+module.exports = {
+    // get all users
+    async getUsers(req, res) {
+      try {
+        const users = await User.find();
+        res.json(users);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+
+    // get a single user by its _id and populated thought and friend data
+    async getSingleUser(req, res) {
+      try {
+        const user = await User.findOne({ _id: req.params.userId })
+          .select('-__v');
+  
+        if (!user) {
+          return res.status(404).json({ message: 'No user with that ID' });
+        }
+  
+        res.json(user);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+
+    // Post or create a new user    
+    async createUser(req, res) {
+      try {
+        const dbUserData = await User.create(req.body);
+        res.json(dbUserData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+    // PUT to update a user by its _id
+    async updateUser(req, res) {
+        try {
+          const users = await User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: req.body },
+            { runValidators: true, new: true }
+          );
+    
+          if (!users) {
+            return res.status(404).json({ message: 'No user with this id!' });
+          }
+    
+          res.json(users);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+    },
+    
+    // DELETE to remove user by its _id
+    async deleteUsers(req, res) {
+        try {
+            const users = await User.findOneAndDelete({ _id: req.params.id });
+      
+            if (!users) {
+              return res.status(404).json({ message: 'No User with that ID' });
+            }
+      
+            await Thought.deleteMany({ _id: { $in: users.users } });
+            res.json({ message: 'Both Thoughts and Users are deleted!' });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+      },
+
+  };
